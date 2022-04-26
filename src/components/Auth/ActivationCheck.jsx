fda7be1d6a3bbabdf49ca11616aeb480278e1daa
Ali <ali.text77@gmail.com>
2022-04-26 23:16:24 +0430

    fix security bugs

--- src/components/Auth/ActivationCheck.jsx
+++ src/components/Auth/ActivationCheck.jsx
@@ -1,16 +1,25 @@
-import { isEmpty, isNull } from 'lodash';
+import { isNull } from 'lodash';
 import React, { Fragment, useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
-import { Navigate, useLocation, useNavigate } from 'react-router';
-import { SyncLoader } from 'react-spinners';
+import { Navigate } from 'react-router';
 
 const ActivationCheck = ({ children }) => {
 
     const user = useSelector(state => state.User);
+    const [navigateTo, setNavigateTo] = useState(null)
+
+    useEffect(() => {
+        if (user.status == 0) {
+           setNavigateTo("/panel/")
+        } else if (isNull(user.email_verified_at)) {
+            setNavigateTo("/panel/profile")
+        }
+    }, [])
+    
 
     return (
         <Fragment>
-            {user.status === 0 ? <Navigate to="/panel/" /> : children}
+            {!isNull(navigateTo) ? <Navigate to={navigateTo} /> : children}
         </Fragment>
     );
 }
