c86ee4e5f49194e7a56ff72c23acc6881ac2dc68
Ali <ali.text77@gmail.com>
2022-05-01 01:58:04 +0430

    fix bug(unmounted components) and update dropdowns

--- src/components/Account/Trades/Trade.jsx
+++ src/components/Account/Trades/Trade.jsx
@@ -1,6 +1,7 @@
 import React, { Fragment, useEffect, useRef, useState } from 'react';
 import listenForOutsideClicks from '../../Services/listenForOutsideClick';
 import moment from 'moment';
+import Dropdown from './Dropdown';
 
 const Trade = ({ setDoUserWantTradeDetails, setDoUserNeedEditTradeWindow, trade, index, setTrade, handleDelTrade }) => {
 
@@ -19,7 +20,7 @@ const Trade = ({ setDoUserWantTradeDetails, setDoUserNeedEditTradeWindow, trade,
         toggleDropDownBtn()
     }
 
-    
+
     return (
         <Fragment>
             <tr className="text-xxs lg:text-base font-light mt-2 py-2 dark:text-slate-300">
@@ -44,28 +45,7 @@ const Trade = ({ setDoUserWantTradeDetails, setDoUserNeedEditTradeWindow, trade,
                         <i className="fa-light fa-angle-down mr-1"></i>
                     </button>
                     {showDropDownBtn && (
-                        <ul
-                            className="absolute top-12 -right-10 lg:top-14 lg:right-auto z-10 drop-shadow-lg bg-slate-100 dark:bg-slate-800 rounded-lg w-24 lg:w-36 overflow-hidden">
-                            <li>
-                                <button onClick={() => handleOpenTradeDetailsWindow()}
-                                    className="w-full text-right text-xxxs lg:text-xs py-2 hover:bg-slate-200 dark:hover:bg-slate-700 transition ease-out duration-300">
-                                    <i className="fa-light fa-memo-circle-info text-xs lg:text-base mx-2"></i>
-                                    جزییات</button>
-                            </li>
-                            <li>
-                                <button onClick={() => handleOpenEditTradeWindow()}
-                                    className="w-full text-right text-xxxs lg:text-xs py-2 hover:bg-slate-200 dark:hover:bg-slate-700 transition ease-out duration-300">
-                                    <i className="fa-light fa-pen-to-square text-xs lg:text-base mx-2"></i>
-                                    ویرایش</button>
-                            </li>
-                            <li>
-                                <button onClick={() => handleDelTrade(trade.account_id, trade.id)}
-                                    className="w-full text-right text-xxxs lg:text-xs py-2 hover:bg-slate-200 dark:hover:bg-slate-700 transition ease-out duration-300">
-                                    <i className="fa-light fa-trash text-xs lg:text-base mx-2"></i>
-                                    حذف</button>
-                            </li>
-
-                        </ul>
+                        <Dropdown toggleDropDownBtn={toggleDropDownBtn} trade_id={trade.id} account_id={trade.account_id} handleDelTrade={handleDelTrade} handleOpenEditTradeWindow={handleOpenEditTradeWindow} handleOpenTradeDetailsWindow={handleOpenTradeDetailsWindow} />
                     )}
 
                 </td>
